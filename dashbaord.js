adf6c05523ae45a3df34165df315175bfc89cd54
GasserElkhodary <gelkhoda@vols.utk.edu>
2025-09-27 00:27:13 -0400

    Update dashbaord.js

--- dashbaord.js
+++ dashbaord.js
@@ -1,5 +1,5 @@
 const statusDiv = document.getElementById('status');
-const socket = new WebSocket("wss://regulations-toronto-feb-zoloft.trycloudflare.com");
+const socket = new WebSocket("const socket = new WebSocket('ws://67.173.188.102:8080');");
 
 const dataElements = {
     rpm: document.getElementById('rpm'),
@@ -153,4 +153,5 @@ socket.onerror = function(error) {
     stopCamera();
 
 };
+
 
